4181a80a84e4b9969586e185bfb8da95679e5ed2
Swapnil Mahadeokar <swapnil.mahadeokar@sumasoft>
2025-11-19 12:16:49 +0530

    Removed commented code and added authorization check

--- roda-ui/roda-wui/src/main/java/org/roda/wui/api/controllers/DiskController.java
+++ roda-ui/roda-wui/src/main/java/org/roda/wui/api/controllers/DiskController.java
@@ -2,37 +2,43 @@ package org.roda.wui.api.controllers;
 
 import java.util.Map;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.roda.core.RodaCoreFactory;
+import org.roda.core.data.exceptions.AuthorizationDeniedException;
 import org.roda.core.data.exceptions.GenericException;
+import org.roda.core.data.exceptions.RODAException;
+import org.roda.core.data.v2.log.LogEntryState;
+import org.roda.core.data.v2.user.User;
+import org.roda.core.model.utils.UserUtility;
 import org.roda.core.storage.StorageService;
+import org.roda.wui.common.ControllerAssistant;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DiskController {
 
-  // @GetMapping("/diskstats")
-  // public Map<String, Object> getDiskStats() throws GenericException,
-  // ClassNotFoundException {
-  //
-  // StorageService storageService = RodaCoreFactory.getStorageService();
-  //
-  // if (storageService instanceof StorageServiceWrapper wrapper) {
-  // StorageService underlying = wrapper.getWrappedStorageService();
-  //
-  // if (underlying instanceof FileStorageService fileStorage) {
-  //
-  // // now you can call your non-static method that uses basePath
-  // return fileStorage.getStorageStats();
-  //
-  // }
-  // }
-  // return null;
-  // }
   @GetMapping("/diskstats")
-  public Map<String, Object> getDiskStats() throws GenericException {
+  public Map<String, Object> getDiskStats(HttpServletRequest request)
+      throws GenericException, AuthorizationDeniedException {
+    final ControllerAssistant controllerAssistant = new ControllerAssistant() {};
+
+    User user = UserUtility.getUser(request);
+
+    // check user permissions
+    controllerAssistant.checkRoles(user);
+
     StorageService storageService = RodaCoreFactory.getStorageService();
-    return storageService.getStorageStats();
+
+    LogEntryState state = LogEntryState.SUCCESS;
+    try {
+      return storageService.getStorageStats();
+    } catch (RODAException e) {
+      state = LogEntryState.FAILURE;
+      throw e;
+    } finally {
+      controllerAssistant.registerAction(user, state);
+    }
   }
 
 }
